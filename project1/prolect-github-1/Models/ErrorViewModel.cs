5b9446930750eb8211488f89ef78332604172958
fatemeh nazaralmolk <fatemeh25239@gmail.com>
2024-02-13 21:16:17 +0330

    mvc app added

---
+++ project1/prolect-github-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prolect_github_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
